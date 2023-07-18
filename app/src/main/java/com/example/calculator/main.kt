07b410108059149e0847e275ca7544df80758227
kwonyongil <kkwonyi95@gmail.com>
2023-07-18 12:12:19 +0900

    git commit -m "level 2 완성?"

--- app/src/main/java/com/example/calculator/main.kt
+++ app/src/main/java/com/example/calculator/main.kt
@@ -10,23 +10,23 @@ class Calculator() {
     fun caculator() {
         println("연습용 텍스트 계산기 입니다.")
         println("첫번째 수를 입력해주세요")
-        var num1 = readLine()!!.toInt()
+        var num1 = readLine()?.toInt()!!
 
         println("두번째 수를 입력해주세요")
-        var num2 = readLine()!!.toInt()
+        var num2 = readLine()?.toInt()!!
 
         println("연산기호를 입력해주세요 (+, - , *, /,%) ")
         var operator = readLine()
 
-        var result = when (operator) {
-            "+" -> num1 + num2
-            "-" -> num1 - num2
-            "*" -> num1 * num2
-            "/" -> num1 / num2
-            "%" -> num1 / num2
-            else -> {
-                println("잘못된 접근입니다 다시 작성해주세요")
-            }
+        var result: String = when (operator) {
+            "+" -> (num1 + num2).toString()
+            "-" -> (num1 - num2).toString()
+            "*" -> (num1 * num2).toString()
+            "/" -> (num1 / num2).toString()
+            "%" -> (num1 % num2).toString()
+            else ->
+                "잘못된 접근입니다 다시 작성해주세요"
+
         }
         println("결과: $result 입니다.")
     }
