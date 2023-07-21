3c144a302380d007ae95f4135930693f94218921
kwonyongil <kkwonyi95@gmail.com>
2023-07-21 19:27:41 +0900

    git commit -m "level4 완성....."

--- app/src/main/java/com/example/calculator/main.kt
+++ app/src/main/java/com/example/calculator/main.kt
@@ -2,32 +2,33 @@ package com.example.calculator
 
 fun main() {
     println("연습용 텍스트 계산기 입니다.")
+
     println("첫번째 수를 입력해주세요")
     var num1 = readLine()?.toInt()!!
+
     println("두번째 수를 입력해주세요")
     var num2 = readLine()?.toInt()!!
+
     println("연산기호를 입력해주세요 (+, - , *, /,%) ")
     var operator = readLine()
 
-    val caculater = Calculator()
+    var result: AbstractOperation =
+        when (operator) {
+            "+" -> AddOperation(num1, num2)
+            "-" -> SubstractOperation(num1, num2)
+            "*" -> MultiplyOperation(num1, num2)
+            "/" -> DivideOperation(num1, num2)
+            else
+               -> AddOperation(num1, num2)
+
+        }
+    val caculater = Calculator(result)
     caculater.caculate()
-
-    val add = AddOperation(num1, num2)
-    add.operate()
-    val substract = SubstractOperation(num1, num2)
-    substract.operate()
-    val multiply = MultiplyOperation(num1, num2)
-    multiply.operate()
-    val divide = DivideOperation(num1, num2)
-    divide.operate()
-
-    val result = caculater.caculate()
-    println("답은{$result}")
+    println(caculater.caculate())
 }
 
-class Calculator(val operator: AbstractOperation) {
+class Calculator(var operator: AbstractOperation) {
 
     fun caculate(): Double = operator.operate()
-
 
 }
